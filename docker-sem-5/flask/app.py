698cab3dbdf8efbcc5a41aa1d49dc3cfb523cc48
NoSignificantWorries <dmitry.karpachev@hotmail.com>
2025-04-25 17:03:44 +0700

    new

--- docker-sem-5/flask/app.py
+++ docker-sem-5/flask/app.py
@@ -1,17 +1,31 @@
-from flask import Flask
+import json
+
+from flask import Flask, jsonify
+from flask import make_response
 import psycopg
 
 app = Flask(__name__)
 
+with open("/app-conf", "r") as config_file:
+    data = json.load(config_file)
+
+with open("/run/secrets/dbpass", "r") as pass_file:
+    password = pass_file.read()
+
 status = "Not connected"
 
-with psycopg.connect("dbname=postgres host=db-proj-db-1 user=postgres password=1234") as conn:
+with psycopg.connect(f"dbname={data['dbname']} host={data['host']} user={data['user']} password={password}") as conn:
     status = "Connected"
+
+
+@app.route("/conf")
+def conf():
+    return jsonify(data)
 
 
 @app.route("/")
 def hello():
-    return f"Hello from container!\nStatus: {status}", 200
+    return make_response(f"Hello from container!\nStatus: {status}", 200)
 
 
 if __name__ == "__main__":
